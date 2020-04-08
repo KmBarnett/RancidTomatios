710b76de9117a9eaacdedcd220dbc1d4e98a80ae
kmbarnett <kmbarnett94@gmail.com>
2020-04-08 14:32:45 -0600

    Add StarSlider

---
+++ src/components/StarSlider/StarSlider.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './StarSlider.css';
+
+
+
+class StarSlider extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      rating: this.props.rating
+    }
+  }
+
+
+  render() {
+    const { rating } = this.state
+    return (
+      <div className="star-range">
+        <input disabled type="range" name="range" min="0" max="10" step="1" list="tickmark" value={rating}/>
+        <div className="star-white star">★★★★★</div>
+        <div style={{width: rating/2 + 'em'}} className="star-black star">★★★★★</div>
+      </div>
+    );
+  }
+}
+
+export default StarSlider;
