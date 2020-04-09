d352ac19a3e62bab9d24b28f284e5d94512710e1
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-09 17:22:05 -0600

    Pass in postMovieRating as props to post a rating

--- src/components/RatingForm/RatingForm.js
+++ src/components/RatingForm/RatingForm.js
@@ -3,8 +3,8 @@ import './RatingForm.css'
 
 
 class RatingForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       rating: 1
     }
@@ -14,17 +14,29 @@ class RatingForm extends Component {
     this.setState({ rating: e.target.value })
   }
 
+  submitRating = event => {
+    event.preventDefault();
+    //if a user has a rating for this movie, delete that rating and give it a new rating, else just give the movie a rating
+    this.props.postMovieRating(this.state.rating)
+  }
+
   render() {
     return(
-      <form>
-      <input
-        type='number'
-        min='1'
-        step='1'
-        max='10'
-        onChange={this.updateRating}/>
-      <button>Add Rating</button>
+      <form onSubmit={this.submitRating}>
+        <input
+          type='number'
+          min='1'
+          step='1'
+          max='10'
+          value={this.state.rating}
+          required
+          onChange={this.updateRating}/>
+        <button
+          type='submit'>Add Rating
+        </button>
       </form>
     )
   }
 }
+
+export default RatingForm;
