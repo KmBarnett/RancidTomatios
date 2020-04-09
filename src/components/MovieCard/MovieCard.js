19b8f07885675b42a3d4a1bfa9982938b192ebba
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-09 14:51:53 -0600

    Merge stash

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './MovieCard.css';
 import StarSlider from './../StarSlider/StarSlider.js';
-import { connect } from 'react-redux';
-
-
 
 const MovieCard = (props) => {
-  console.log(`${props.userRating}: ${props.average_rating}`);
-
   return(
     <article className='card'>
       <img className='card-img' src={props.poster_path} alt={props.title} />
@@ -18,14 +13,11 @@ const MovieCard = (props) => {
         <section>
           <p>Average Rating:</p>
           <StarSlider rating = {props.average_rating}/>
+          {props.userRating && <StarSlider rating={props.userRating.rating} />}
         </section>
       </section>
     </article>
   )
 }
 
-const mapStateToProps = (state, {id}) => ({
-  userRating: state.user.ratings && state.user.ratings.find(rating => rating.movie_id === id)
-})
-
-export default connect(mapStateToProps)(MovieCard);
+export default MovieCard;
