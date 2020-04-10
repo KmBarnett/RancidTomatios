016376da5b8ddb5a938de8530ff6d57510c42348
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-09 19:21:13 -0600

    Create function that can delete a previous rating

--- src/components/MovieShowPage/MovieShowPage.js
+++ src/components/MovieShowPage/MovieShowPage.js
@@ -10,38 +10,50 @@ const MovieShowPage = (props) => {
   const { movie = {} } = props
   const rating = props.userRating ?
     props.userRating.rating : 0
+
+  const removePreviousRating = () => {
+    const ratingId = props.user.ratings.find(rating => rating.movie_id === movie.id).id
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${props.user.id}/ratings/${ratingId}`, {
+      method: 'DELETE'
+    })
+      .catch(error => console.error(error))
+  }
+
   const postMovieRating = rating => {
-      fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${props.user.id}/ratings`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          movie_id: movie.id,
-          rating: parseInt(rating)
-        })
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${props.user.id}/ratings`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        movie_id: movie.id,
+        rating: parseInt(rating)
       })
+    })
       .then(response => response.json())
       .then(data => props.rateMovie(data.rating))
-    }
+  }
 
-  return(
+  return (
     <article className='show-page'>
       <img src={movie.backdrop_path} />
       <h2>{movie.title}</h2>
       <h3>Released: {moment(movie.release_date).format("MMMM Do YYYY")}</h3>
       <section>
         <p>Average Rating:</p>
-        <StarSlider rating={movie.average_rating}/>
+        <StarSlider rating={movie.average_rating} />
       </section>
       {props.user.name &&
         <section>
-        <p>Your Rating:</p>
+          <p>Your Rating:</p>
           <div>
             <StarSlider rating={rating} />
-            <RatingForm postMovieRating={postMovieRating} />
+            <RatingForm
+              removePreviousRating={removePreviousRating}
+              postMovieRating={postMovieRating}
+              rating={rating} />
           </div>
-      </section>}
+        </section>}
       <p>{movie.overview}</p>
     </article>
   )
@@ -58,7 +70,7 @@ const mapStateToProps = (state, { matchId }) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  rateMovie: rating => dispatch( rateMovie(rating) )
+  rateMovie: rating => dispatch(rateMovie(rating))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(MovieShowPage);
