9f083de29d7f67102b1b82834158fab816c562d7
kmbarnett <kmbarnett94@gmail.com>
2020-04-13 14:25:33 -0600

    Restyle back button

--- src/containers/MovieShowPage/MovieShowPage.js
+++ src/containers/MovieShowPage/MovieShowPage.js
@@ -15,7 +15,7 @@ const MovieShowPage = (props) => {
 
   const removePreviousRating = () => {
     const ratingId = props.user.ratings.find(rating => rating.movie_id === movie.id).id
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${props.user.id}/ratings/${ratingId}`, {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${props.user.id}/ratings/${ratingId}`, {
       method: 'DELETE'
     })
       .catch(error => console.error(error))
@@ -45,7 +45,7 @@ const MovieShowPage = (props) => {
       <img className='backdrop-img' src={movie.backdrop_path} />
       <section className='movie-info'>
         <Link to='/'>
-          <button className='back-button' type='button'>⬅</button>
+          <button className='back-button' type='button'>⬅ Back</button>
         </Link>
         <h2>{movie.title}</h2>
         <img className='poster-path-img' src={movie.poster_path} alt={movie.title} />
