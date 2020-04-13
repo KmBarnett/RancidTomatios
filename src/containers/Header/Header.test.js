23d86e01d442fbef6a044a8a3add8a8a43f6d433
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-13 17:15:00 -0600

    Add rendering and general structure for testing Header component

---
+++ src/containers/Header/Header.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from './Header';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
+
+const testStore = createStore(rootReducer);
+function renderHeader(user = {}, logOut = jest.fn()) {
+  return render(
+    <Provider store={testStore}>
+      <BrowserRouter>
+        <Header user={user} logOut={logOut}/>
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+describe('Header', () => {
+  it('should render the correct content', () => {
+    const { getByText, queryByText } = renderHeader();
+    const loginBtn = getByText('LOG IN');
+    const logOutBtn = queryByText('Log out');
+
+    expect(loginBtn).toBeInTheDocument();
+    expect(logOutBtn).not.toBeInTheDocument();
+  });
+  
+  it('should render the correct content', () => {
+    
+  });
+
+})
