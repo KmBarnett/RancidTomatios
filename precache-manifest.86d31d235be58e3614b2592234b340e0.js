1ebacb835b0766033b39eaeac71cd970b2a51801
kmbarnett <kmbarnett94@gmail.com>
2020-05-07 10:37:46 -0600

    Updates

---
+++ precache-manifest.86d31d235be58e3614b2592234b340e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4f2e1531590c4d96d48df2d713c408",
+    "url": "/RancidTomatios/index.html"
+  },
+  {
+    "revision": "1e5c595fd31d56281b7b",
+    "url": "/RancidTomatios/static/css/main.a3342441.chunk.css"
+  },
+  {
+    "revision": "d0528e2fe98b9ba12be6",
+    "url": "/RancidTomatios/static/js/2.0b0ad17c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RancidTomatios/static/js/2.0b0ad17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5c595fd31d56281b7b",
+    "url": "/RancidTomatios/static/js/main.31f90adf.chunk.js"
+  },
+  {
+    "revision": "2fe53b2d0de8a0ad32ff",
+    "url": "/RancidTomatios/static/js/runtime-main.141bbd59.js"
+  },
+  {
+    "revision": "7e7f044e17f7fedf84eb4c3086ebafcf",
+    "url": "/RancidTomatios/static/media/Logo.7e7f044e.svg"
+  }
+]);
